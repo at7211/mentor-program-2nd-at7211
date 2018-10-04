3496b5a6c0cb35a88f3dbc1b9ea5ae2f73b65ae2
at7211 <at7211@gmail.com>
2018-10-04 14:38:26 +0800

    hw1 finished

---
+++ homeworks/week4/hw1/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let q = document.querySelector;
+    let ac = q('#ac')
+    alert(q('#ac'))
+
+
+
+
+
+
+
+
+})
+
+q('.btn__row').addEventListener('click', e => {
+
+})
