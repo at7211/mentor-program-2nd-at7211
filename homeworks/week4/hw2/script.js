46e9cc634f12861972076edb03d552322320e63e
at7211 <at7211@gmail.com>
2018-10-09 11:55:55 +0800

    hw4 finished

--- homeworks/week4/hw2/script.js
+++ homeworks/week4/hw2/script.js
@@ -1,7 +1,7 @@
 document.getElementById("submit").addEventListener('click', e => {
-    result();
-    e.preventDefault();
 
+    e.preventDefault();
+    result();
 
 })
 
@@ -11,31 +11,36 @@ function result() {
     let email = document.getElementById("email")
     let nickname = document.getElementsByName("nickname")
     let occupation = document.getElementsByTagName('occupation')
-    let input = document.getElementsByName("submit")
-    let type1 = document.getElementById('type1');
-    let type2 = document.getElementById('type2');
     let coding_experience = document.getElementsByName('coding_experience')
     let reminder = document.getElementsByClassName("reminder")
+    let answer = ''
 
 
-    if (email.value === "" || nickname.value === "" ||
-        occupation.value === "" || coding_experience.value === "") {
-        for (let i = 0; i < test.length; i++) {
-            test[i].style.backgroundColor = '#ffd6d6';
-            reminder[i].style.color = '#db4437';
-            
-        }
-        
-    } else if (!type1.checked && !type2.checked) {
-        test[i].style.backgroundColor = '#ffd6d6';
-        reminder[i].style.color = '#db4437';
-        
+
+    if (email.value === "") {
+        test[0].style.backgroundColor = '#ffd6d6';
+        reminder[0].style.color = '#db4437';
+    } else if (nickname.value === "") {
+        test[1].style.backgroundColor = '#ffd6d6';
+        reminder[1].style.color = '#db4437';
+    } else if (document.querySelector('input[name=class_type]:checked') === null) {
+        test[2].style.backgroundColor = '#ffd6d6';
+        reminder[2].style.color = '#db4437';
+    } else if (occupation.value === "") {
+        test[3].style.backgroundColor = '#ffd6d6';
+        reminder[3].style.color = '#db4437';
+    } else if (coding_experience.value === "") {
+        test[4].style.backgroundColor = '#ffd6d6';
+        reminder[4].style.color = '#db4437';
     } else {
-        console.log(email.value);
-        console.log(nickname.value);
-        console.log(occupation.value);
-        console.log(coding_experience.value);
-        alert("提交表單！")
-        
+        answer += "您填寫的內容如下" + "\r\n"
+        answer += "電子郵件： "+ email.value + "\n"
+        answer += "暱稱： "+ nickname.value + "\n"  
+        answer += "報名類型： "+ document.querySelector('input[name=class_type]:checked').value + "\n"
+        answer += "現在的職業： "+ occupation.value + "\n"
+        answer += "是否有程式相關背景？： "+ coding_experience.value + "\n"
+        answer += "其他： "+ document.querySelector('input[name=others').value + "\r\n"
+        console.log(answer)
+        alert("提交表單！");
     }
 }
--- homeworks/week4/hw5.md
+++ homeworks/week4/hw5.md
@@ -1,6 +1,7 @@
 ## 什麼是 DOM？
 
-DOM 的全名為 Document Object Model，透過『節點』的方式將網頁以『結構化』的方式呈現，並使開發者可以存取、更動網頁任一節點的架構、內容、屬性、樣式等。
+DOM 的全名為 Document Object Model，由瀏覽器提供，以『結構樹』的方式呈現資料結構，並使開發者可以存取、更動網頁任一節點的架構、內容、屬性、樣式等。
+因此是JS與html之間的橋樑。
 
 
 ## 什麼是 Ajax？
@@ -27,35 +28,29 @@ method是HTTP協定中定義一組指令可以針對網頁執行特定操作。�
 ## GET 跟 POST 有哪些區別，可以試著舉幾個例子嗎？
 
 get:用於請求伺服器發送某個資源。
-post：新增一項資料。（如果存在會新增一個新的）
+post：新增一項資料。（如果存在會新增一個新的）。
 
-
+但最顯著的差別在於get在傳送資料給sever時會將輸入的資料顯示在網址列，反之 post 則不會，因此多數的登入系統因都採用post method提高安全性。
 
 
 ## 什麼是 RESTful API？
-RESTful API是一種設計風格，這種風格使API設計具有整體一致性，易於維護、擴展。並讓人（串接 API 或使用 API 的人）可以很快速的了解你的 API 
+RESTful API是一種設計風格，這種風格使API設計具有整體一致性，易於維護、擴展。並讓人（串接 API 或使用 API 的人）可以很快速的了解你的 API。 
 
 ## JSON 是什麼？
 一種呈現js物件的資料格式，閱讀易理解，常用於 Ajax 的資料交換格式。
 
 ##JSONP 是什麼？
-JSONP (（JSON with Padding）是解決跨網域限制的方法之一，利用script標籤不受同源政策的限制，可以跨網域的特性，從別的網域取得資料。
+JSONP (JSON with Padding）是解決跨網域限制的方法之一，利用script標籤不受同源政策的限制，可以跨網域的特性，從別的網域取得資料。
 
 ## 要如何存取跨網域的 API？
 
-有兩種方式：
+有兩種方式，一種為 JSONP，一種為 CORS。
 
-- JSONP：如上述。
-缺點：只能使用 GET method，參數必須帶在網址上。
-
-- CORS 跨來源資源共享 (cross-origin resource sharing)：
-
-是一份瀏覽器技術的規範，透過添加額外的 HTTP header 使網站取得存取其他網域伺服器特定資源的權限。
+前者如上述，後者為一份瀏覽器技術的規範，透過添加額外的 HTTP header 使網站取得存取其他網域伺服器特定資源的權限。
 
 當請求不同網域的資源時，會建立一個跨來源 HTTP 請求（cross-origin HTTP request）。所以想跨網域存取的話，Server 必須在 Response 的 Header 裡面加上`Access-Control-Allow-Origin`。當瀏覽器收到 Response 後，若檢查到裡面有包含發起 Request 的 Origin 的話，就會允許通過，讓程式順利接收到 Response。
 
-舉例：`Access-Control-Allow-Origin: *`  `*` 代表接受任何一個 Origin。
+用比喻來說伺服器就像建立一個通關密語（`Access-Control-Allow-Origin`），唯有知曉通關密語的人才得以擁有權限允許通過（Requset有對應的Origin）
 
-此外，與 JSONP 不同，CORS 除了 GET 以外也支援其他的 HTTP 請求。
+目前主流已少用使用 JSONP，主要原因是即是建立起 CORS。另外CORS可以支援其他的 HTTP請求（JSONP只能使用 GET method）
 
-### 以上兩種跨網域存取方式，都要確保 Server 端有加上 `Access-Control-Allow-Origin` 才可以確保存取。
